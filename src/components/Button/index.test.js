1dac4dd3f870c7aab2b1e35ec19ddad26eaafd99
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-02-12 11:45:21 +0530

    components: adding testid to button

--- src/components/Button/index.test.js
+++ src/components/Button/index.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {Button} from './index';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 describe('the button component', () => {
     it('should render correctly', () => {
-        const { asFragment } = render(<Button />);
+        const { asFragment } = render(<Button testID="test-btn">Test text</Button>);
 
         expect(asFragment()).toMatchSnapshot();
     });
+
+    // it('should render correctly', () => {
+    //     const { getByTestId } = render(<Button>Test text</Button>);
+
+    //     expect(asFragment()).toMatchSnapshot();
+    // });
 })
